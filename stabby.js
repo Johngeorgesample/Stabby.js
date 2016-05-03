d63543499708412e76ae89e7fc1bfcda9770eca9
John-George Sample <Johngeorgesample@gmail.com>
2016-05-03 13:25:48 -0400

    Create stabby.js

---
+++ stabby.js
@@ -0,0 +1,18 @@
+/*
+stabby.js™ is the brainchild of SRU's Web Communications. This framework is going to revolutionize the Web industry at a time in the near future. stabby.js™ is one of multiple intended frameworks for the SUX™ (Surprise User eXperience™) developmental concept. 
+*/
+
+function stabUser() {
+	console.log("Stab!");
+}
+
+//Don't (do) use this cause document.write "sux"!
+function stabUserPage() {
+	document.write("Stab!");
+}
+
+function buttonStab() {
+alert("OMG Stabbed AGAIN!");
+}
+
+
